e62ba0b26975dbdb4a65969660df1df7eb04d4d1
Mqtth3w <mqtth3w@duck.com>
2025-01-19 12:59:36 +0100

    Add files via upload

--- unet/solver.py
+++ unet/solver.py
@@ -7,21 +7,23 @@ import torch
 import torch.optim as optim
 import torch.nn as nn
 import os
-import numpy as np
 
 from tqdm import tqdm
 from model import Net
 
-# Define a dice loss functions
-def iou_loss(pred, target):
-    smooth = 1e-6
+# Define loss/help functions
+def iou(pred, target):
+    smooth = 1e-6 # avoid zero division
     intersection = (pred * target).sum()
     union = pred.sum() + target.sum() - intersection
     iou = (intersection + smooth) / (union + smooth)
-    return 1 - iou
+    return iou
+
+def iou_loss(pred, target):
+    return 1 - iou(pred, target)
 
 def dc_loss(pred, target):
-    smooth = 1e-6
+    smooth = 1e-6 # avoid zero division
     predf = pred.view(-1)
     targetf = target.view(-1)
     intersection = (predf * targetf).sum()
@@ -47,7 +49,7 @@ class Solver(object):
         
         # Define Loss function
         if self.args.loss == "BCE": # not the bank, lol
-            self.criterion = nn.BCEWithLogitsLoss()
+            self.criterion = nn.BCELoss()
         elif self.args.loss == "dice":
             self.criterion = dc_loss()
         elif self.args.loss == "iou":
@@ -63,7 +65,6 @@ class Solver(object):
         elif self.args.opt == "SGD":
             self.optimizer = optim.SGD(self.net.parameters(), lr=self.args.lr, momentum=0.9, foreach=True)
         
-
         self.epochs = self.args.epochs
         self.train_loader = train_loader
         self.test_loader = test_loader
@@ -85,88 +86,25 @@ class Solver(object):
         print("Model loaded!")
     
     def train(self):
-
-        # Keep track of average training and test losses for each epoch
-        avg_train_losses = []
-        avg_test_losses = []
-        
-        # Trigger for earlystopping
-        earlystopping = False 
-
+        self.net.train()
         for epoch in range(self.epochs):  # loop over the dataset multiple times
 
-            # Record the training and test losses for each batch in this epoch
-            train_losses = []
-            test_losses = []
             running_loss = 0.0
-
-            self.net.train()
-
             loop = tqdm(enumerate(self.train_loader), total = len(self.train_loader), leave = False)
-            for batch, (images, targets) in loop:
+            for batch, (imgs, masks) in loop:
+                # put data on correct device
+                imgs = imgs.to(self.device)
+                masks = masks.to(self.device) # the ground truth mask
 
-                images = images.to(self.device)
-                targets = targets.to(self.device) # the ground truth mask
-                
                 # zero the parameter gradients
-                self.optimizer.zero_grad()
-
                 # forward + backward + optimize
-                pred = self.net(images)
-                loss = self.criterion(pred, targets)
+                self.net.zero_grad()
+                preds = self.net(imgs)
+                loss = self.criterion(preds, masks)
                 loss.backward()
                 self.optimizer.step()
 
-                train_losses.append(loss.item())
                 running_loss += loss.item()
-
-                # print statistics
-                if batch % self.args.print_every == self.args.print_every - 1:  
-                    
-                    print(f'[{epoch + 1}, {batch + 1:5d}] loss: {running_loss / self.args.print_every:.3f}')
-
-                    self.writer.add_scalar('training loss',
-                        running_loss / self.args.print_every,
-                        epoch * len(self.train_loader) + batch)
-                    
-                    running_loss = 0.0
-
-                    # Test model ####################??????????
-                    self.test(epoch, batch)
-
-            self.save_model()
-        
-            ##############
-            self.net.eval()
-            
-            with torch.no_grad():     # Record and print average validation loss for each epoch 
-                for test_batch, (test_images, test_targets) in enumerate(self.test_loader):
-                    test_images = test_images.to(self.device)
-                    test_targets = test_targets.to(self.device)
-                    test_pred = self.net(test_images.detach())
-
-                    test_loss = dc_loss(test_pred, test_targets).item()
-
-                    test_losses.append(test_loss)
-
-                epoch_avg_train_loss = np.mean(train_losses)
-                epoch_avg_test_loss = np.mean(test_losses)
-                avg_train_losses.append(epoch_avg_train_loss)
-                avg_test_losses.append(epoch_avg_test_loss)
-
-                print_msg = (f'train_loss: {epoch_avg_train_loss:.5f} ' + f'valid_loss: {epoch_avg_test_loss:.5f}')
-
-                print(print_msg)
-            
-            if epoch > 10:     #Early stopping with a patience of 1 and a minimum of 10 epochs 
-                if avg_test_losses[-1]<=avg_test_losses[-2]:
-                    print("Early Stopping Triggered With Patience 1")
-                    torch.save(self.net.state_dict(), self.args.checkpoint_path)
-                    earlystopping = True 
-            if earlystopping:
-                break
-
-        #return  avg_train_losses, avg_test_losses
 
 
 
@@ -174,30 +112,51 @@ class Solver(object):
         self.writer.close()
         print('Finished Training')   
     
-    def test(self, epoch, i):
+    def test(self, epoch, batch):
         # now lets evaluate the model on the test set
-        correct = 0
-        total = 0
+        # Intersection over Union, L1 distance are good metrics to evaluate the results
+        # init loss and iou, l1 metrics
+        test_loss = 0.0
+        tot_iou = 0.0
+        tot_l1_distance = 0.0
 
         # put net into evaluation mode
         self.net.eval()
 
         # since we're not training, we don't need to calculate the gradients for our outputs
         with torch.no_grad():
-            for data in self.test_loader:
-                inputs, labels = data
+            for (imgs, masks) in self.test_loader:
+                
                 # put data on correct device
-                inputs, labels = inputs.to(self.device), labels.to(self.device)
+                imgs, masks = imgs.to(self.device), masks.to(self.device)
                 # calculate outputs by running images through the network
-                outputs = self.net(inputs)
-                # the class with the highest energy is what we choose as prediction
-                _, predicted = torch.max(outputs.data, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
+                preds = self.net(imgs)
+                
+                # test loss
+                loss = self.criterion(preds, masks)
+                test_loss += loss.item()
 
-        self.writer.add_scalar('test accuracy',
-            100 * correct / total,
-            epoch * len(self.train_loader) + i)
+                # metrics!
+                tot_iou += iou(preds, masks)
 
-        print(f'Accuracy of the network on the 10000 test images: {100 * correct / total} %')
+                l1_distance = torch.abs(preds - masks).mean()
+                tot_l1_distance += l1_distance.item()
+
+        # print and log statistics
+        num_batches = len(self.test_loader)
+        avg_test_loss = test_loss / num_batches
+        avg_iou = tot_iou / num_batches
+        avg_l1_distance = tot_l1_distance / num_batches
+
+        self.writer.add_scalar('Test Loss (avg on batches)', 
+                               avg_test_loss, epoch * num_batches + batch)
+        self.writer.add_scalar('IoU (avg on batches)', 
+                               avg_iou, epoch * num_batches + batch)
+        self.writer.add_scalar('L1 Distance (avg on batches)', 
+                               avg_l1_distance, epoch * num_batches + batch)
+
+        print(f"Epoch {epoch}, Batch {batch}:")
+        print(f"Avg test Loss: {avg_test_loss:.4f}.")
+        print(f"Avg IoU: {avg_iou:.4f}.")
+        print(f"Avg L1 Distance: {avg_l1_distance:.4f}.\n")
         self.net.train()
