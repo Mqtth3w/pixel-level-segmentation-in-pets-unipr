946b91594df8d773d1e44e13a456a9d537c58ec4
Mqtth3w <mqtth3w@duck.com>
2025-01-22 12:12:05 +0100

    Update hpc_DLAGM.bash

--- unet/hpc_DLAGM.bash
+++ unet/hpc_DLAGM.bash
@@ -24,31 +24,31 @@ conda activate mqtth3w
 # Adam, dice loss
 python ./main.py --dataset_path /hpc/archive/T_2024_DLAGM/matteo.gianvenuti/ \
 --checkpoint_path /hpc/group/T_2024_DLAGM/matteo.gianvenuti/checkpoints \
---run_name UNet_Adam_dice_lr_1_e5 --model_mame UNet_Adam_dice_lr_1_e5
+--run_name UNet_Adam_dice_lr_1_e5 --model_name UNet_Adam_dice_lr_1_e5
 
 # Adam, BCE loss
 python ./main.py --dataset_path /hpc/archive/T_2024_DLAGM/matteo.gianvenuti/ \
 --checkpoint_path /hpc/group/T_2024_DLAGM/matteo.gianvenuti/checkpoints \
---run_name UNet_Adam_BCE_lr_1_e5 --model_mame UNet_Adam_BCE_lr_1_e5 --loss BCE
+--run_name UNet_Adam_BCE_lr_1_e5 --model_name UNet_Adam_BCE_lr_1_e5 --loss BCE
 
 # Adam, combo loss
 python ./main.py --dataset_path /hpc/archive/T_2024_DLAGM/matteo.gianvenuti/ \
 --checkpoint_path /hpc/group/T_2024_DLAGM/matteo.gianvenuti/checkpoints \
---run_name UNet_Adam_combo_lr_1_e5 --model_mame UNet_Adam_combo_lr_1_e5 --loss combo
+--run_name UNet_Adam_combo_lr_1_e5 --model_name UNet_Adam_combo_lr_1_e5 --loss combo
 
 # RSMprop, dice loss
 python ./main.py --dataset_path /hpc/archive/T_2024_DLAGM/matteo.gianvenuti/ \
 --checkpoint_path /hpc/group/T_2024_DLAGM/matteo.gianvenuti/checkpoints \
---run_name UNet_RSMprop_dice_lr_1_e5 --model_mame UNet_RSMprop_dice_lr_1_e5 --opt RSMprop
+--run_name UNet_RSMprop_dice_lr_1_e5 --model_name UNet_RSMprop_dice_lr_1_e5 --opt RSMprop
 
 # RSMprop, BCE loss
 python ./main.py --dataset_path /hpc/archive/T_2024_DLAGM/matteo.gianvenuti/ \
 --checkpoint_path /hpc/group/T_2024_DLAGM/matteo.gianvenuti/checkpoints \
---run_name UNet_RSMprop_BCE_lr_1_e5 --model_mame UNet_RSMprop_BCE_lr_1_e5 --opt RSMprop --loss BCE
+--run_name UNet_RSMprop_BCE_lr_1_e5 --model_name UNet_RSMprop_BCE_lr_1_e5 --opt RSMprop --loss BCE
 
 # RSMprop, combo loss
 python ./main.py --dataset_path /hpc/archive/T_2024_DLAGM/matteo.gianvenuti/ \
 --checkpoint_path /hpc/group/T_2024_DLAGM/matteo.gianvenuti/checkpoints \
---run_name UNet_RSMprop_combo_lr_1_e5 --model_mame UNet_RSMprop_combo_lr_1_e5 --opt RSMprop --loss combo
+--run_name UNet_RSMprop_combo_lr_1_e5 --model_name UNet_RSMprop_combo_lr_1_e5 --opt RSMprop --loss combo
 
 conda deactivate
