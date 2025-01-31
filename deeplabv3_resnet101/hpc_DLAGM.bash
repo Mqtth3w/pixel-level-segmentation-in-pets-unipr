e86cad1955fbe502ee5b71362fc764b1b469ac4e
Mqtth3w <mqtth3w@duck.com>
2025-01-31 10:48:52 +0100

    Add files via upload

--- deeplabv3_resnet101/hpc_DLAGM.bash
+++ deeplabv3_resnet101/hpc_DLAGM.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:a100_40g:1
+#SBATCH --gres=gpu:a100_80g:1
 #SBATCH --qos=gpu
 #SBATCH --time 23:59:59
-#SBATCH --mem=64gb
+#SBATCH --mem=80gb
 #SBATCH --ntasks-per-node 10
 
 #SBATCH --mail-type=ALL
@@ -29,8 +29,6 @@ conda activate mqtth3w
 # and it works better with higher resolution imgs then since it recovers execution time 
 # by not testing again loss and similar I use it to work on higher resolution images
 
-COMMENT
-
 # learning rate tests ("grid" search)
 # lr 2e-4
 python ./main.py --dataset_path /hpc/archive/T_2024_DLAGM/matteo.gianvenuti/ \
@@ -52,5 +50,19 @@ python ./main.py --dataset_path /hpc/archive/T_2024_DLAGM/matteo.gianvenuti/ \
 python ./main.py --dataset_path /hpc/archive/T_2024_DLAGM/matteo.gianvenuti/ \
 --checkpoint_path /hpc/group/T_2024_DLAGM/matteo.gianvenuti/checkpoints \
 --run_name DeepLabV3ResNet_lr7e_5 --model_name DeepLabV3ResNet_lr7e_5 --lr 7e-5
+
+# The best IoU 0.8935 and L1 distance 0.0387, achieved by DeepLabV3ResNet_lr5e_4
+# Check best_res_1run.txt to see the best results of each run
+
+COMMENT
+
+# Weight decay tests (after each run I always set the new top values as default values)
+python ./main.py --dataset_path /hpc/archive/T_2024_DLAGM/matteo.gianvenuti/ \
+--checkpoint_path /hpc/group/T_2024_DLAGM/matteo.gianvenuti/checkpoints \
+--run_name DeepLabV3ResNet_wd1e_5 --model_name DeepLabV3ResNet_wd1e_5 --weight_decay 1e-5
+
+python ./main.py --dataset_path /hpc/archive/T_2024_DLAGM/matteo.gianvenuti/ \
+--checkpoint_path /hpc/group/T_2024_DLAGM/matteo.gianvenuti/checkpoints \
+--run_name DeepLabV3ResNet_wd1e_4 --model_name DeepLabV3ResNet_wd1e_4 --weight_decay 1e-4
 
 conda deactivate
